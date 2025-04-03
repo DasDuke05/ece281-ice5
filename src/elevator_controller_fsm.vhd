cb24482c77790161a7650f083bc4945ad9bf6dd0
DasDuke05 <177964866+DasDuke05@users.noreply.github.com>
2025-04-03 14:36:24 -0600

    it is finished

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -95,13 +95,37 @@ begin
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	
 	-- Next State Logic
-  
+    f_Q_next <= s_floor2 when (f_Q = s_floor1 AND i_up_down = '1') else -- going up
+                s_floor3 when (f_Q = s_floor2 AND i_up_down = '1') else 
+                s_floor4 when (f_Q = s_floor3 AND i_up_down = '1') else
+                s_floor4 when (f_Q = s_floor4 AND i_up_down = '1') else -- top floor reached
+                s_floor3 when (f_Q = s_floor4 AND i_up_down = '0') else -- going down
+                s_floor2 when (f_Q = s_floor3 AND i_up_down = '0') else
+                s_floor1 when (f_Q = s_floor2 AND i_up_down = '0') else
+                s_floor1 when (f_Q = s_floor1 AND i_up_down = '0') else
+                s_floor1;
 	-- Output logic
-
+    with f_Q select
+    o_floor <=  "0001" when s_floor1,
+                "0010" when s_floor2,
+                "0011" when s_floor3,
+                "0100" when s_floor4,
+                "0001" when others; -- default is floor1
 	-------------------------------------------------------------------------------------------------------
 	
 	-- PROCESSES ------------------------------------------------------------------------------------------	
-	
+	register_proc : process (i_clk, i_reset)
+    begin
+        if (rising_edge(i_clk)) then
+            if i_reset = '1' then
+                f_Q <= s_floor2;
+            elsif i_stop = '1' then
+                f_Q <= f_Q;
+            else
+                f_Q <= f_Q_next;
+            end if;    -- next state becomes current state
+        end if;
+    end process register_proc;
 	-- State register ------------
 	
 	
